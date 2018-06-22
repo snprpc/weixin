a4fed4028989684e7f203f1c4dd75435100c8378
snprpc <603254109@qq.com>
2018-06-22 04:34:50 +0000

    添加测试文本消息接口

---
+++ app/Http/Controllers/MsgController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use EasyWeChat\Factory;
+class MsgController extends Controller
+{
+    //
+
+    public function text() {
+        // 一些配置
+        $config = [
+            'app_id' => 'wx8ac5d4e87ef72f88',
+            'secret' => '8afd403fec8b641825ed5b59961d81aa',
+
+            'response_type' => 'array',
+
+            'log' => [
+              'level' => 'debug',
+              'file' => __DIR__.'/../storage/logs/wechat.log',
+            ],
+        ];
+        // 使用配置来初始化一个公众号应用实例。
+        $app =  Factory::officialAccount($config);
+        $app->server->push(function(){
+            use EasyWeChat\Kernel\Messages\Text;
+            // or
+            $text = new Text();
+            $text->content = '您好！overtrue。';
+            return $text;
+        }
+    }
+}
