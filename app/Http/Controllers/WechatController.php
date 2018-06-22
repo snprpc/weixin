4342fd2905b8530a115ce5d47e34e62641d9dad1
snprpc <603254109@qq.com>
2018-06-22 07:02:09 +0000

    测试消息接口

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -16,11 +16,14 @@ class WechatController extends Controller
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $app = app('wechat.official_account');
+        // 关注成功信息
         $app->server->push(function($message){
-            return "欢迎关注 overtrue！";
+            return $message;
         });
 
         return $app->server->serve();
     }
+
+
 
 }
