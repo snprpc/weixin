286383c4bc1108f6d2788d50448f64f97487843f
snprpc <603254109@qq.com>
2018-06-22 05:10:23 +0000

    测试恢复消息

--- routes/web.php
+++ routes/web.php
@@ -19,29 +19,29 @@ Route::post('/test_post/print','MsgController@test_post');
 
 
 
-Route::get('/server',function(){
-    // 一些配置
-    $config = [
-        'app_id' => 'wx8ac5d4e87ef72f88',
-        'secret' => '8afd403fec8b641825ed5b59961d81aa',
-
-        'response_type' => 'array',
-
-        'log' => [
-          'level' => 'debug',
-          'file' => __DIR__.'/../storage/logs/wechat.log',
-        ],
-    ];
-    // 使用配置来初始化一个公众号应用实例。
-    $app =  EasyWeChat\Factory::officialAccount($config);
-    $response = $app->server->serve();
-
-    // 将响应输出
-    return $response;
-});
+// Route::get('/server',function(){
+//     // 一些配置
+//     $config = [
+//         'app_id' => 'wx8ac5d4e87ef72f88',
+//         'secret' => '8afd403fec8b641825ed5b59961d81aa',
+//
+//         'response_type' => 'array',
+//
+//         'log' => [
+//           'level' => 'debug',
+//           'file' => __DIR__.'/../storage/logs/wechat.log',
+//         ],
+//     ];
+//     // 使用配置来初始化一个公众号应用实例。
+//     $app =  EasyWeChat\Factory::officialAccount($config);
+//     $response = $app->server->serve();
+//
+//     // 将响应输出
+//     return $response;
+// });
 
 // 消息接口
-Route::post('/msg/text','MsgController@text');
+Route::post('/server','MsgController@text');
 
 Route::get('/', function () {
     return view('welcome');
