8d1694e5a8cb974e90c636819ba8dfed2c8599d8
snprpc <603254109@qq.com>
2018-06-22 10:32:58 +0000

    设置菜单

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -57,19 +57,19 @@ class WechatController extends Controller
             "name"      =>  "文章",
             "sub_button"=>  [
                 [
-                    "type":"click",
-                    "name":"影评",
-                    "key": "yingping"
+                    "type"  =>  "click",
+                    "name"  =>  "影评",
+                    "key"   =>  "yingping"
                 ],
                 [
-                    "type":"click",
-                    "name":"散文",
-                    "key":"sanwen"
+                    "type"  =>  "click",
+                    "name"  =>  "散文",
+                    "key"   =>  "sanwen"
                 ],
                 [
-                    "type":"click",
-                    "name":"微小说",
-                    "key":"weixiaoshuo"
+                    "type"  =>  "click",
+                    "name"  =>  "微小说",
+                    "key"   =>  "weixiaoshuo"
                 ],
             ],
         ],
@@ -77,14 +77,14 @@ class WechatController extends Controller
             "name"      =>  "活动",
             "sub_button"=>  [
                 [
-                    "type": "click",
-                    "name": "线下活动",
-                    "key": "xianxiahuodong"
+                    "type"  =>  "click",
+                    "name"  =>  "线下活动",
+                    "key"   =>  "xianxiahuodong"
                 ],
                 [
-                    "type": "click",
-                    "name": "互动",
-                    "key": "hudong"
+                    "type"  =>  "click",
+                    "name"  =>  "互动",
+                    "key"   =>  "hudong"
                 ],
             ],
         ],
@@ -92,14 +92,14 @@ class WechatController extends Controller
             "name"      =>  "关于我们",
             "sub_button"=>  [
                 [
-                    "type": "click",
-                    "name": "线下活动",
-                    "key": "xianxiahuodong"
+                    "type"  =>  "click",
+                    "name"  =>  "线下活动",
+                    "key"   =>  "xianxiahuodong"
                 ],
                 [
-                    "type": "click",
-                    "name": "商业合作",
-                    "key": "shangyehuodong"
+                    "type"  =>  "click",
+                    "name"  =>  "商业合作",
+                    "key"   =>  "shangyehuodong"
                 ],
             ],
         ],
