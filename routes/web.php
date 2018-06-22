8f0d617902c05b8966638a638f90920f666ceacd
snprpc <603254109@qq.com>
2018-06-22 06:41:36 +0000

    配置token验证

--- routes/web.php
+++ routes/web.php
@@ -17,29 +17,10 @@ Route::get('/test_post',function(){
 Route::post('/test_post/print','MsgController@test_post');
 
 
-// Route::get('/server',function(){
-//     // 一些配置
-//     $config = [
-//         'app_id' => 'wx8ac5d4e87ef72f88',
-//         'secret' => '8afd403fec8b641825ed5b59961d81aa',
-//
-//         'response_type' => 'array',
-//
-//         'log' => [
-//           'level' => 'debug',
-//           'file' => __DIR__.'/../storage/logs/wechat.log',
-//         ],
-//     ];
-//     // 使用配置来初始化一个公众号应用实例。
-//     $app =  EasyWeChat\Factory::officialAccount($config);
-//     $response = $app->server->serve();
-//
-//     // 将响应输出
-//     return $response;
-// });
+Route::any('/server','WechatController@server');
 
 // 消息接口
-Route::post('/server','MsgController@text');
+// Route::post('/server','MsgController@text');
 
 Route::get('/', function () {
     return view('welcome');
